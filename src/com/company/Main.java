ea33001de4f6ed7010ea20d5d8ffefac2fe136b0
NoToNiezlaBeczka <sylwestermuras@gmail.com>
2019-12-14 12:04:50 +0100

    xcD

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
+int a = 0;
+int b = 5;
     public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 	System.out.println("Poda swoje imie:");
